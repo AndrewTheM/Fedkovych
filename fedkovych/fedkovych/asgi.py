07e274eb794e07b764a5403642e18e6e05cb7852
Andrew <andrewmakoviichuk@gmail.com>
2024-08-19 15:23:04 +0300

    Added Django project

---
+++ fedkovych/fedkovych/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for fedkovych project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fedkovych.settings')
+
+application = get_asgi_application()
