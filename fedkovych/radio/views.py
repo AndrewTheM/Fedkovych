a5020100d1b8b4fdd26719e989205bed2c621ced
Andrew <andrewmakoviichuk@gmail.com>
2024-08-23 16:34:36 +0300

    Implemented track title rotation

--- fedkovych/radio/views.py
+++ fedkovych/radio/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+import os
+from pathlib import Path
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
 
 def stream_player(request):
     return render(request, 'index.html')
+
+def radio_schedule(request):
+    schedule_file = os.path.join(os.path.dirname(os.getcwd()), 'music-bot', 'radio_schedule.txt')
+    with open(schedule_file, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+    return JsonResponse({'schedule': lines})
