289997035698a53b994662e948b2800fd80c1736
SerhiiP02 <sergijpavcuk@gmail.com>
2024-08-28 17:01:33 +0300

    slider creation

--- fedkovych/static/main.js
+++ fedkovych/static/main.js
@@ -1,22 +1,8 @@
-const playButton = document.getElementById('radio-play');
-const pauseButton = document.getElementById('radio-pause');
 const currentTrack = document.getElementById('current-track');
-const radioPlayer = document.getElementById('radio-player');
-const videoPlayer = document.getElementById('video-player');
-const signalSlider = document.getElementById('signal-slider');
-const signalBars = document.querySelectorAll('.signal-bar');
 
 const delay = 15;
 
 window.addEventListener('load', updateCurrentTrack);
-signalSlider.addEventListener('input', updateVolume);
-playButton.addEventListener('click', playRadio);
-pauseButton.addEventListener('click', pauseRadio);
-videoPlayer.addEventListener('play', pauseRadio);
-
-// Initialize volume slider from local storage
-signalSlider.value = parseInt(localStorage.getItem('volume'));
-updateSignalBars(signalSlider.value);
 
 function updateCurrentTrack() {
     fetch('/api/radio_schedule')
@@ -43,33 +29,4 @@ function updateCurrentTrack() {
         }, 1000);
     })
     .catch(error => console.error('Error fetching data:', error));
-}
-
-function updateVolume() {
-    radioPlayer.volume = signalSlider.value / 5;
-    localStorage.setItem('volume', signalSlider.value.toString());
-    updateSignalBars(signalSlider.value);
-}
-
-function updateSignalBars(level) {
-    signalBars.forEach((bar, index) => {
-        if (index < level) {
-            bar.classList.add('active');
-        } else {
-            bar.classList.remove('active');
-        }
-    });
-}
-
-function playRadio() {
-    radioPlayer.play();
-    videoPlayer.pause();
-    playButton.hidden = true;
-    pauseButton.hidden = false;
-}
-
-function pauseRadio() {
-    radioPlayer.pause();
-    playButton.hidden = false;
-    pauseButton.hidden = true;
 }
