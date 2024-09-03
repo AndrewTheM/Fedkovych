0f10c7ac0b630335841740e0310f73a47df240b4
Andrew <andrewmakoviichuk@gmail.com>
2024-09-03 18:50:15 +0300

    Adapted music bot for cloud storage

--- music-bot/main.py
+++ music-bot/main.py
@@ -5,8 +5,9 @@ import psycopg2
 import random
 import time as t
 from dotenv import load_dotenv
+from google.cloud import storage
 
-load_dotenv('../fedkovych/.env')
+load_dotenv()
 
 base_directory = 'music/'
 media_queue_file = 'media_queue.txt'
@@ -20,6 +21,8 @@ db_config = {
     'host': os.getenv('DATABASE_HOST'),
 }
 
+gcloud_bucket = os.getenv('GS_BUCKET_NAME')
+
 
 async def create_random_media_queue():
     conn = psycopg2.connect(**db_config)
@@ -30,16 +33,18 @@ async def create_random_media_queue():
     conn.close()
     random.shuffle(media_info)
     
-    with open(f'{os.getcwd()}/{media_queue_file}', 'w', encoding='utf-8') as file:
+    with open(media_queue_file, 'w', encoding='utf-8') as file:
         file.write('ffconcat version 1.0\n')
         for info in media_info:
             file.write(f'file {info[1]}\n')
     
     cur_time = int(t.time())
-    with open(f'{os.getcwd()}/{radio_schedule_file}', 'w', encoding='utf-8') as file:
+    with open(radio_schedule_file, 'w', encoding='utf-8') as file:
         for info in media_info:
             cur_time += int(info[2])
             file.write(f'{cur_time} {info[0]}\n')
+    
+    gcloud_upload(radio_schedule_file)
 
 async def process_files():
     with open(f'{os.getcwd()}/{radio_schedule_file}', 'w') as file:
@@ -51,17 +56,35 @@ async def process_files():
     media_info = [row[0] for row in cur.fetchall()]
     
     for file in media_info:
+        gcloud_download(file)
         probe = ffmpeg.probe(file)
         stream = next((stream for stream in probe['streams']), None)
         duration = int(float(stream['duration']))
         convert_video(file)
+        gcloud_upload(file)
         cur.execute(f'UPDATE radio_video SET is_converted = True, duration_seconds = %s WHERE mp4_file = %s', (duration, file))
     
     if len(media_info) > 0:
         conn.commit()
+    
     cur.close()
     conn.close()
-    
+
+# Google Cloud
+
+def gcloud_download(file):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(gcloud_bucket)
+    blob = bucket.blob(file)
+    blob.download_to_filename(file)
+
+def gcloud_upload(file):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(gcloud_bucket)
+    blob = bucket.blob(file)
+    blob.upload_from_filename(file)
+
+# FFmpeg
 
 def stream_queue():
     (
